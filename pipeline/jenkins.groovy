fb73d1d7f7a117cf54f60897fbafaecb1036c485
Maxim Derbenev <maksym.derbenov@hit.zone>
2023-05-26 05:15:32 -0400

    groovy

---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,50 @@
+pipeline {
+agent any
+    environment {
+        REPO = "https://github.com/mirik12/kbot"
+        BRANCH = 'main'
+    }
+    stages {
+
+        stage ("clone") {
+            steps {
+            echo 'CLONE REPOSITORY'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+       }
+
+        stage ("test") {
+            steps {
+                sh "make --version"
+                echo 'TEST EXECUTION STARTED '
+                sh 'make test'
+            }
+       }
+
+        stage ("build") {
+            steps {
+                echo 'BUILD EXECUTION STARTED '
+                sh 'make build'
+            }
+       }
+
+        stage ("image") {
+            steps {
+                script {
+                    echo 'TEST EXECUTION STARTED '
+                    sh 'make image'
+                }
+            }
+       }
+
+        stage ("push") {
+            steps {
+                script {
+                    docker.withRegistry ( '', 'dockerhub ') {
+                    sh 'make push'
+                    }
+                }
+            }
+        }
+    }
+}
